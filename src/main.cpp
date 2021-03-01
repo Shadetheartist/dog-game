cf7eec1926236ec309f764ecfff23e1ee0167500
Derek <shadetheartist@gmail.com>
2021-03-01 12:55:20 -0800

    added splash screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include "nice-boys.h"
 
 TFT_eSPI tft = TFT_eSPI(135, 240);
 
 void setup(void) {
+  
   tft.init();
   tft.setRotation(3);
+  tft.setSwapBytes(true);
+  tft.pushImage(0,0,240,135, niceboys);
 }
 
 int x = 0, y = 0;
 
 void loop() {
-  tft.fillScreen(TFT_BLACK);
-  tft.fillRect(x++, y++, 4, 4, 0xfff);
-  delay(32);
+  //this sets the backlight duty cycle
+  digitalWrite(4, LOW);
+  delay(9);
+  digitalWrite(4, HIGH);
+  delay(1);
 }
 
